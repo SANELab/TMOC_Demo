e291049bb1221beee3e9cd42b4afe6f6747d39e9
SANELab <39320756+SANELab@users.noreply.github.com>
2022-01-19 16:07:22 +0900

    Delete dbconn.php

--- dbconn.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysql_host = "163.152.126.96";
-$mysql_port = "3306";
-$mysql_user = "root";	# ID
-$mysql_password = "Qwerty1!";	# PW
-$mysql_db = "user_db";			# DB name
-
-$dbconn = mysqli_connect($mysql_host, $mysql_user, $mysql_password, $mysql_db, $mysql_port) or die("DB connect failed.");
-?>
