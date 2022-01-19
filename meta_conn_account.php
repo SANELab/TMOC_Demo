82fbef1dac4a5a5e66bc95b77bf7ee2ac49272d6
SANELab <39320756+SANELab@users.noreply.github.com>
2022-01-19 16:09:06 +0900

    Delete meta_conn_account.php

--- meta_conn_account.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-  include('dbconn.php');
-  $id=$_SESSION['id'];
-  $sql="SELECT u_walletaddress FROM members WHERE u_id='".$id."'"; // 쿼리 넣는걸로 변경
-  $result=mysqli_query($dbconn, $sql);
-  $record=mysqli_fetch_array($result);
-?>
